e58e58be72eceb1b3d6c2b28640586102a9a614f
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-06 16:53:21 +0200

    Refine test cases

--- test/ConvertTest.php
+++ test/ConvertTest.php
@@ -4,27 +4,25 @@ class ConvertTest extends \PHPUnit_Framework_TestCase
 {
     public function testConvertWithMissingParameters()
     {
+        $_REQUEST['content'] = 'bla';
+        $this->expectOutputRegex("/.*Missing parameter.*/");
+        require __DIR__.'/../convert.php';
+        unset($_REQUEST['content']);
+        $_REQUEST['to'] = 'json';
         $this->expectOutputRegex("/.*Missing parameter.*/");
         require __DIR__.'/../convert.php';
     }
     public function testConvertWithIllegalValue()
     {
         $this->expectOutputRegex("/.*Illegal value.*/");
+        $_REQUEST['content'] = 'bla';
         $_REQUEST['to'] = 'illegal';
         require __DIR__.'/../convert.php';
     }
     public function testConvertToJSON()
     {
         $_REQUEST['to'] = 'json';
-        $_FILES = array(
-            array(
-                'name' => 'simpleBiom.hdf5',
-                'type' => 'application/octet-stream',
-                'size' => 33840,
-                'tmp_name' => __DIR__ . '/files/simpleBiom.hdf5',
-                'error' => 0
-            )
-        );
+        $_REQUEST['content'] = file_get_contents(__DIR__ . '/files/simpleBiom.hdf5');
         $this->expectOutputRegex("/.*Biological Observation Matrix 1.0.0.*/");
         require __DIR__.'/../convert.php';
     }
