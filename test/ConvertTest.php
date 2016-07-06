ed22fe1ef856e93b6845882d681af7a3d910fb88
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-06 16:10:24 +0200

    Add test for illegal value

--- test/ConvertTest.php
+++ test/ConvertTest.php
@@ -7,4 +7,10 @@ class ConvertTest extends \PHPUnit_Framework_TestCase
         $this->expectOutputRegex("/.*Missing parameter.*/");
         require __DIR__.'/../convert.php';
     }
+    public function testConvertWithIllegalValue()
+    {
+        $this->expectOutputRegex("/.*Illegal value.*/");
+        $_REQUEST['to'] = 'illegal';
+        require __DIR__.'/../convert.php';
+    }
 }
