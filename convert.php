def6edef26f33d155a3533f7a6e96642978e8ef4
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-06 16:10:59 +0200

    Add error for illegal values

--- convert.php
+++ convert.php
@@ -2,4 +2,6 @@
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 if(!isset($_REQUEST['to'])){
     print json_encode(array("error" => "Missing parameter 'to' please set to 'json' or 'hdf5'"));
+} elseif ($_REQUEST['to'] !== 'json' && $_REQUEST['to'] !== 'hdf5'){
+    print json_encode(array("error" => "Illegal value of 'to' parameter please set to 'json' or 'hdf5'"));
 }
