79b6e3e29b1fbb1b41378b59bc957956f48f16d7
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-06 12:02:23 +0200

    Add assertions to test case

--- test/biomcs/BiomCSTest.php
+++ test/biomcs/BiomCSTest.php
@@ -23,5 +23,9 @@ class BiomCSTest extends \PHPUnit_Framework_TestCase
         $results_obj = json_decode($results, true);
         // var_dump($results_obj);
         $this->assertEquals("b'No Table ID'", $results_obj["id"]);
+        $this->assertEquals("Biological Observation Matrix 1.0.0", $results_obj["format"]);
+        $this->assertEquals(array(3,1,12), $results_obj["data"][1]);
+        $this->assertEquals("OTU_8", $results_obj["rows"][7]["id"]);
+        $this->assertEquals("Sample_3", $results_obj["columns"][2]["id"]);
     }
 }
