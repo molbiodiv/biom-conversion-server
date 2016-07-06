afc79d452cf96c6fddae783e2c93270d8d27d420
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-06 16:13:40 +0200

    Add test case for convert to JSON

--- test/ConvertTest.php
+++ test/ConvertTest.php
@@ -13,4 +13,19 @@ class ConvertTest extends \PHPUnit_Framework_TestCase
         $_REQUEST['to'] = 'illegal';
         require __DIR__.'/../convert.php';
     }
+    public function testConvertToJSON()
+    {
+        $_REQUEST['to'] = 'json';
+        $_FILES = array(
+            array(
+                'name' => 'simpleBiom.hdf5',
+                'type' => 'application/octet-stream',
+                'size' => 33840,
+                'tmp_name' => __DIR__ . '/files/simpleBiom.hdf5',
+                'error' => 0
+            )
+        );
+        $this->expectOutputRegex("/.*Biological Observation Matrix 1.0.0.*/");
+        require __DIR__.'/../convert.php';
+    }
 }
