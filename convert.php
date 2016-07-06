9fe245622cba6a5c691dc3aa39079515dce8c31a
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-06 16:53:41 +0200

    Fix implementation to convert to json

--- convert.php
+++ convert.php
@@ -1,7 +1,14 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-if(!isset($_REQUEST['to'])){
+$to = isset($_REQUEST["to"]) ? $_REQUEST["to"] : false;
+$content = isset($_REQUEST["content"]) ? $_REQUEST["content"] : false;
+if(!$to || !$content){
     print json_encode(array("error" => "Missing parameter 'to' please set to 'json' or 'hdf5'"));
-} elseif ($_REQUEST['to'] !== 'json' && $_REQUEST['to'] !== 'hdf5'){
+} elseif ($to !== 'json' && $to !== 'hdf5'){
     print json_encode(array("error" => "Illegal value of 'to' parameter please set to 'json' or 'hdf5'"));
+} else {
+    $biomcs = new \biomcs\BiomCS();
+    if($to === "json"){
+        print $biomcs->convertToJSON($content);
+    }
 }
