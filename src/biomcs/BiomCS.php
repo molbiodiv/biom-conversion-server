018ec2d41c8aa2613fe449b307b187df72dbf512
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-06 14:33:23 +0200

    Add code for convertToHDF5

--- src/biomcs/BiomCS.php
+++ src/biomcs/BiomCS.php
@@ -16,7 +16,24 @@ class BiomCS
         $json_string = "";
         if ($write_file !== false) {
             $this->executeBiomCommand("--to-json", $temp_file);
-            $json_string = file_get_contents($temp_file.".json");
+            $json_string = file_get_contents($temp_file.".out");
+        }
+        return $json_string;
+    }
+
+    /**
+     * Converts the content of a biom file to hdf5
+     * @param $content mixed - content of a biom file to be converted to hdf5 (e.g. json)
+     * @return string        - hdf5 encoded biom object
+     */
+    public function convertToHDF5($content)
+    {
+        $temp_file = tempnam(sys_get_temp_dir(), 'biomcs');
+        $write_file = file_put_contents($temp_file, $content);
+        $json_string = "";
+        if ($write_file !== false) {
+            $this->executeBiomCommand("--to-hdf5", $temp_file);
+            $json_string = file_get_contents($temp_file.".out");
         }
         return $json_string;
     }
@@ -33,7 +50,7 @@ class BiomCS
         $result = array();
         $errorCode = 0;
         exec('biom convert -i '.escapeshellarg($filename).
-                ' -o '.escapeshellarg($filename).'.json '.escapeshellarg($parameter), $result, $errorCode);
+                ' -o '.escapeshellarg($filename).'.out '.escapeshellarg($parameter), $result, $errorCode);
         if ($errorCode !== 0) {
             throw new \Exception("Could not execute biom command: ".$result." ".$errorCode);
         }
