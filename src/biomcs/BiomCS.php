43d56d8e173ace513729acded2847b5bb6524bb5
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-06 11:38:00 +0200

    Add convertToJSON skeleton

--- src/biomcs/BiomCS.php
+++ src/biomcs/BiomCS.php
@@ -11,5 +11,12 @@ namespace biomcs;
 
 class BiomCS
 {
-
+    /**
+     * Converts the content of a biom file to json
+     * @param $content content of a biom file that should be converted to json (e.g. binary content of a hdf5 file)
+     * @return string json encoded biom object
+     */
+    public function convertToJSON($content){
+        return "";
+    }
 }
