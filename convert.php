80974484bcb479346b8d413cb2bfe4cdd8590ef4
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-07 14:17:44 +0200

    Improve error message for missing parameter (more specific)

--- convert.php
+++ convert.php
@@ -16,10 +16,12 @@
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 $to = isset($_REQUEST["to"]) ? $_REQUEST["to"] : false;
 $content = isset($_REQUEST["content"]) ? $_REQUEST["content"] : false;
-if (!$to || !$content) {
+if (!$to) {
     print json_encode(array("error" => "Missing parameter 'to' please set to 'json' or 'hdf5'"));
 } elseif ($to !== 'json' && $to !== 'hdf5') {
     print json_encode(array("error" => "Illegal value of 'to' parameter please set to 'json' or 'hdf5'"));
+} elseif (!$content) {
+    print json_encode(array("error" => "Missing parameter 'content' please send the content to convert"));
 } else {
     $biomcs = new \biomcs\BiomCS();
     if ($to === "json") {
