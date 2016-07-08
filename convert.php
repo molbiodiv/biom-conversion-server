b5060896b8fbc4768ac3eabfe476a3c380b8b362
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-08 09:26:08 +0200

    Add proper exception handling

--- convert.php
+++ convert.php
@@ -17,28 +17,32 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 $to = isset($_REQUEST["to"]) ? $_REQUEST["to"] : false;
 $content = isset($_REQUEST["content"]) ? $_REQUEST["content"] : false;
 if (!$to) {
-    print json_encode(array("error" => "Missing parameter 'to' please set to 'json' or 'hdf5'"));
+    echo json_encode(array("error" => "Missing parameter 'to' please set to 'json' or 'hdf5'"));
 } elseif ($to !== 'json' && $to !== 'hdf5') {
-    print json_encode(array("error" => "Illegal value of 'to' parameter please set to 'json' or 'hdf5'"));
+    echo json_encode(array("error" => "Illegal value of 'to' parameter please set to 'json' or 'hdf5'"));
 } elseif (!$content) {
-    print json_encode(array("error" => "Missing parameter 'content' please send the content to convert"));
+    echo json_encode(array("error" => "Missing parameter 'content' please send the content to convert"));
 } else {
     $biomcs = new \biomcs\BiomCS();
-    if ($to === "json") {
-        print json_encode(
-            array(
-                "content" => json_decode($biomcs->convertToJSON($content), true),
-                "error" => null
-            ),
-            JSON_PRETTY_PRINT
-        );
-    } elseif ($to === "hdf5") {
-        print json_encode(
-            array(
-                "content" => base64_encode($biomcs->convertToHDF5($content)),
-                "error" => null
-            ),
-            JSON_PRETTY_PRINT
-        );
+    try{
+        if ($to === "json") {
+            echo json_encode(
+                array(
+                    "content" => json_decode($biomcs->convertToJSON($content), true),
+                    "error" => null
+                ),
+                JSON_PRETTY_PRINT
+            );
+        } elseif ($to === "hdf5") {
+            print json_encode(
+                array(
+                    "content" => base64_encode($biomcs->convertToHDF5($content)),
+                    "error" => null
+                ),
+                JSON_PRETTY_PRINT
+            );
+        }
+    } catch (Exception $e) {
+        echo json_encode(array("error" => $e->getMessage()), JSON_PRETTY_PRINT);
     }
 }
