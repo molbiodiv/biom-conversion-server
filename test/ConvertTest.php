7b2ce190ca0632a58874ccb2af7d0ccd19dd47c9
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-07-28 08:38:28 +0200

    Fix usage of phpunit in namespaced fashion

--- test/ConvertTest.php
+++ test/ConvertTest.php
@@ -4,7 +4,7 @@ namespace test;
 
 require_once __DIR__.'/overload_header_function.php';
 
-class ConvertTest extends \PHPUnit_Framework_TestCase
+class ConvertTest extends \PHPUnit\Framework\TestCase
 {
     public function testConvertWithMissingTo()
     {
--- test/biomcs/BiomCSTest.php
+++ test/biomcs/BiomCSTest.php
@@ -4,7 +4,7 @@ namespace biomcs;
 
 require_once __DIR__.'/../overload_header_function.php';
 
-class BiomCSTest extends \PHPUnit_Framework_TestCase
+class BiomCSTest extends \PHPUnit\Framework\TestCase
 {
     public function testConvertToJSON()
     {
